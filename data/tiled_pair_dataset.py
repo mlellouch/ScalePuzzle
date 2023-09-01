fc24ca3744882fe5fbdf34f669a16c1fc5639045
Michael Lellouch <michael.lellouch@gmail.com>
2023-09-02 02:26:57 +0300

    first

---
+++ data/tiled_pair_dataset.py
@@ -0,0 +1,95 @@
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path
+from PIL import Image
+import numpy as np
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+
+
+
+class TiledPairDataset(Dataset):
+
+    def __init__(self, images_path: Path, patch_size=32):
+        self.images_path = images_path
+        self.all_images = list(self.images_path.iterdir())
+        self.patch_size = patch_size
+        self.transforms = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    def __len__(self):
+        return len(self.all_images)
+
+    def _get_random_non_neighbors(self, image):
+        center_y = np.random.randint(low=1, high=image.shape[0] // self.patch_size)
+        center_x = np.random.randint(low=1, high=image.shape[1] // self.patch_size)
+
+        non_neighbor_y = np.random.randint(low=1, high=image.shape[0] // self.patch_size)
+        non_neighbor_x = np.random.randint(low=1, high=image.shape[1] // self.patch_size)
+
+        while (abs(non_neighbor_x - center_x) <= 1) and (abs(non_neighbor_y - center_y) <= 1):
+            non_neighbor_y = np.random.randint(low=1, high=image.shape[0] // self.patch_size)
+            non_neighbor_x = np.random.randint(low=1, high=image.shape[1] // self.patch_size)
+
+        center_patch, non_neighbor_patch = self.get_patch(image, (center_y, center_x)), \
+                                           self.get_patch(image, (non_neighbor_y, non_neighbor_x))
+
+        return self._get_tiled_image(center_patch=center_patch, tiled_patch=non_neighbor_patch)
+
+
+    def _get_tiled_image(self, center_patch, tiled_patch):
+        canvas = np.zeros(shape=[self.patch_size * 3, self.patch_size * 3, 3], dtype=center_patch.dtype)
+        canvas[self.patch_size: self.patch_size * 2, self.patch_size: self.patch_size * 2] = center_patch
+
+        canvas[:self.patch_size, self.patch_size: self.patch_size * 2] = tiled_patch
+        canvas[self.patch_size*2:, self.patch_size: self.patch_size * 2] = tiled_patch
+        canvas[self.patch_size: self.patch_size * 2, :self.patch_size] = tiled_patch
+        canvas[self.patch_size: self.patch_size * 2, self.patch_size * 2:] = tiled_patch
+
+        return canvas
+
+    def get_patch(self, image, patch_coords):
+        x, y = patch_coords
+        return image[y * self.patch_size: y * self.patch_size + self.patch_size,
+                       x * self.patch_size: x * self.patch_size + self.patch_size]
+
+
+    def __getitem__(self, idx):
+        image = np.array(Image.open(self.all_images[idx]))
+        chosen_y = np.random.randint(low=1, high=(image.shape[0] // self.patch_size) - 1)
+        chosen_x = np.random.randint(low=1, high=(image.shape[1] // self.patch_size) - 1)
+
+        center_patch = self.get_patch(image, (chosen_y, chosen_x))
+
+        neighbors = {
+            1: (chosen_y - 1, chosen_x),
+            2: (chosen_y + 1, chosen_x),
+            3: (chosen_y, chosen_x - 1),
+            4: (chosen_y, chosen_x + 1)
+        }
+
+        outputs = []
+        labels = []
+        for label, second_patch in neighbors.items():
+            labels.append(label)
+            tiled_patch = self.get_patch(image, second_patch)
+            outputs.append(self._get_tiled_image(center_patch=center_patch, tiled_patch=tiled_patch))
+
+        for _ in range(4):
+            labels.append(0)
+            outputs.append(self._get_random_non_neighbors(image))
+
+        final_images = [self.transforms(Image.fromarray(output_image)) for output_image in outputs]
+        return torch.stack(final_images), torch.tensor(labels, dtype=torch.long)
+
+
+if __name__ == '__main__':
+    # test
+    d = TiledPairDataset(images_path=Path('./images/train'))
+
+    while True:
+        a = d[0]
---
+++ train_pair.py
@@ -0,0 +1,125 @@
+import torch
+import torchvision
+from pathlib import Path
+from torch.utils.data import DataLoader
+import csv
+from data.tiled_pair_dataset import TiledPairDataset
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_resnet(num_classes=5):
+    resnet = torchvision.models.resnet34(weights=torchvision.models.ResNet34_Weights.IMAGENET1K_V1)
+
+    # Substitute the FC output layer
+    resnet.fc = torch.nn.Linear(resnet.fc.in_features, num_classes)
+    torch.nn.init.xavier_uniform_(resnet.fc.weight)
+    return resnet
+
+
+class Trainer:
+
+    def load_model(self):
+        model_path = self.model_log_path / Path('model.pt')
+        if model_path.exists():
+            self.net.load_state_dict(torch.load(model_path))
+            self.net.eval()
+
+    def save_model(self):
+        torch.save(self.net.state_dict(), self.model_log_path / Path('model.pt'))
+
+    def __init__(self, model_log_path: Path, num_classes=5):
+        self.net = get_resnet(num_classes=num_classes)
+        self.model_log_path = model_log_path
+        self.model_log_path.mkdir(exist_ok=True, parents=True)
+        self.criterion = None
+        self.optimizer = None
+
+    def train_epoch(self, train_dataloader):
+        self.net.train()
+        loss_sum = 0
+        for data in train_dataloader:
+            inputs, labels = data
+            # flatten
+            inputs = inputs.view([-1] + list(inputs.shape[2:]))
+            labels = labels.flatten()
+            self.optimizer.zero_grad()
+            prediction = self.net(inputs.to(device=device))
+            loss = self.criterion(prediction, labels.to(device=device))
+            loss_sum += loss.item()
+            loss.backward()
+            self.optimizer.step()
+
+        return loss_sum / len(train_dataloader)
+
+    def test_epoch(self, test_dataloader):
+        self.net.eval()
+        loss_sum = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in test_dataloader:
+                inputs, labels = data
+                inputs = inputs.view([-1] + list(inputs.shape[2:]))
+                labels = labels.flatten()
+
+                outputs = self.net(inputs.to(device=device))
+                loss = self.criterion(outputs, labels.to(device=device))
+                loss_sum += loss.item()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        return loss_sum / len(test_dataloader), correct / total
+
+
+    def setup_training(self):
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
+
+    def get_data(self):
+        train_dataloader = DataLoader(
+            TiledPairDataset(Path('./data/images/train')),
+            batch_size=4,
+            shuffle=True
+        )
+
+        test_dataloader = DataLoader(
+            TiledPairDataset(Path('./data/images/test')),
+            batch_size=4,
+            shuffle=True
+        )
+
+        return train_dataloader, test_dataloader
+
+    def train(self, epochs: int):
+        self.load_model()
+        self.setup_training()
+        train_dataloader, test_dataloader = self.get_data()
+
+        with open(self.model_log_path / Path('trace.csv'), 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=['epoch', 'train_loss', 'test_loss', 'test_accuracy'])
+            writer.writeheader()
+            for epoch in tqdm(range(epochs)):
+                train_loss = self.train_epoch(train_dataloader=train_dataloader)
+                test_loss, test_accuracy = self.test_epoch(test_dataloader=test_dataloader)
+                writer.writerow({
+                    'epoch': epoch,
+                    'train_loss': train_loss,
+                    'test_loss': test_loss,
+                    'test_accuracy': test_accuracy
+                })
+
+        self.save_model()
+
+if __name__ == '__main__':
+    Trainer(model_log_path=Path('./models/first_test')).train(epochs=100)
+
+
+
